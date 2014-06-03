1cbd4324d439a745ef925a0f2cfc7fda8685f9d8
Phillip Tennen <phillip.ennen@gmail.com>
2014-06-03 15:31:18 -0600

    Initial Commit

---
+++ CumulusTests/CumulusTests.swift
@@ -0,0 +1,35 @@
+//
+//  CumulusTests.swift
+//  CumulusTests
+//
+//  Created by Phillip Tennen on 6/3/14.
+//  Copyright (c) 2014 Phillip Tennen. All rights reserved.
+//
+
+import XCTest
+
+class CumulusTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
